2adcbfc813ba5aa81726f7d9a536d96e5fbabe74
AKASH-ROYAL <rosegold060402@gmail.com>
2023-09-15 16:20:36 +0530

    Filter the data's according to the roles

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:trainingproject/testPurpose.dart';
-import 'package:trainingproject/view/06082023.dart';
 import 'package:trainingproject/view/07092023.dart';
-import 'package:trainingproject/view/Functions.dart';
-import 'package:trainingproject/view/tooltipbundle.dart';
-
-import 'view/TaskTwoResult.dart';
-import 'view/myFirstApp.dart';
 
 void main() {
   runApp(MaterialApp(
